3afeb5beaba814e2f1dea47677aadc136f29f903
DucManh2103 <124852371+DucManh2103@users.noreply.github.com>
2023-04-04 14:06:15 +0700

    Add files via upload

---
+++ CTDL&GT_binary_search_2.cpp
@@ -0,0 +1,4 @@
+Input : arr[] = {15, 20, 25, 30, 31, 44, 66}
+        x = 25;
+Output : 2
+//https://freetuts.net/thuat-toan-tim-kiem-nhi-phan-2634.html?fbclid=IwAR3hfUftZ8lXcykuJlULfJLRKXue2UP_4-SyRYP-KEF5TIyIkoYu2-fzxEc
